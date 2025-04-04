9b69c7028d48f834aed848011b16db011a981d99
benoit <bntnowak@outlook.com>
2025-04-04 16:50:15 +0200

    FEAT/ Add LogIn page, add router react  package

--- Co_Voyageur/co_voyageur.client/src/main.jsx
+++ Co_Voyageur/co_voyageur.client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import HomePage from './Views/HomePage/HomePage.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LogInPage from './Views/LogInPage/LogInPage.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>,
+  <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LogInPage />} />
+            </Routes>
+  </Router>
 )
