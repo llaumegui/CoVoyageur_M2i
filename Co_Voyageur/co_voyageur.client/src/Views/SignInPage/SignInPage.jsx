82b85e6a2def6cc44d5d7fe347ddce1f13d728f3
benoit <bntnowak@outlook.com>
2025-04-07 11:30:48 +0200

    FEAT/ add SignInPage

--- Co_Voyageur/co_voyageur.client/src/Views/SignInPage/SignInPage.jsx
+++ Co_Voyageur/co_voyageur.client/src/Views/SignInPage/SignInPage.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import NavBar from '../../Components/NavBar/NavBar';
-import './LogInPage.css';
+import './SignInPage.css';
 const LogInPage = () => {
     return (
         <>
         <NavBar></NavBar>
-        <section className='login-form'>
+        <section className='signIn-form'>
                 <form action="">
-                    <h1>Log In</h1>
+                    <h1>Sign in</h1>
                     <input type="text" placeholder='Username'/>
                     <input type="password" placeholder='Password'/>
-                    <button>Log In</button>
-                    <a href='/signup'>Pas encore de compte? Inscrivez vous!</a>
+                    <input type="password" placeholder='Confirm Password' />
+                    <button>Sign In</button>
+                    <a href='/login'>Déjà incrit? Connectez vous!</a>
                 </form>
             </section>
         </>
--- Co_Voyageur/co_voyageur.client/src/main.jsx
+++ Co_Voyageur/co_voyageur.client/src/main.jsx
@@ -3,12 +3,13 @@ import './index.css'
 import HomePage from './Views/HomePage/HomePage.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LogInPage from './Views/LogInPage/LogInPage.jsx';
-
+import SignInPage from './Views/SignInPage/SignInPage.jsx';
 createRoot(document.getElementById('root')).render(
   <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LogInPage />} />
+                <Route path="/signup" element={<SignInPage />} />
             </Routes>
   </Router>
 )
