37c6b2a80bb5fded17f0017db8739eeef6c0d6ba
benoit <bntnowak@outlook.com>
2025-04-07 09:23:56 +0200

    FIX/ Update Navbar

--- Co_Voyageur/co_voyageur.client/src/Components/NavBar/NavBar.jsx
+++ Co_Voyageur/co_voyageur.client/src/Components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
             <img src="../Images/Logo.png" alt="Logo" className="navbar-logo" />
             <div className='navbar-right'>
                 <a href="#">Rechercher</a>
-                <img src="../Images/Account.png" alt="Logo" className="navbar-account" />
+               <button><img src="../Images/Account.png" alt="Logo" className="navbar-account"/></button> 
             </div>
 
         </nav>
