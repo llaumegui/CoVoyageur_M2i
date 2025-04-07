cdef0231b821841def90412b150beba646138228
benoit <bntnowak@outlook.com>
2025-04-07 11:19:54 +0200

    FEAT/ Add LogInPage

--- Co_Voyageur/co_voyageur.client/src/Views/LogInPage/LogInPage.jsx
+++ Co_Voyageur/co_voyageur.client/src/Views/LogInPage/LogInPage.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import NavBar from '../../Components/NavBar/NavBar';
+import './LogInPage.css';
 const LogInPage = () => {
     return (
         <>
         <NavBar></NavBar>
-            <h1>Log In</h1>
-            <input type="text" placeholder='Username'/>
-            <input type="password" placeholder='Password'/>
-            <button>Log In</button>
+        <section className='login-form'>
+                <form action="">
+                    <h1>Log In</h1>
+                    <input type="text" placeholder='Username'/>
+                    <input type="password" placeholder='Password'/>
+                    <button>Log In</button>
+                    <a href='/'>Pas encore de compte? Inscrivez vous!</a>
+                </form>
+            </section>
         </>
     );
 };
