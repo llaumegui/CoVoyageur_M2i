29358aa8b3ae9ef6441a9b2cfae9871b505495be
benoit <bntnowak@outlook.com>
2025-04-07 14:53:29 +0200

    FEAT/ Add random suggestion group, add Suggestion Page, add Blob component

--- Co_Voyageur/co_voyageur.client/src/Views/SearchPage/SearchPage.jsx
+++ Co_Voyageur/co_voyageur.client/src/Views/SearchPage/SearchPage.jsx
@@ -3,6 +3,7 @@ import SearchBar from '../../Components/SearchBar/SearchBar.jsx';
 import NavBar from '../../Components/NavBar/NavBar.jsx';
 import './SearchPage.css';
 import BackgroundBlob from '../../Components/BackgroundBlob/BackgroundBlob.jsx';
+import Suggestions from '../../Components/Suggestions/Suggestions.jsx';
 const SearchPage = () => {
     return (
         <>
@@ -11,6 +12,7 @@ const SearchPage = () => {
             <div className='SearchBar-container'>
             <SearchBar></SearchBar>
             </div>
+            <Suggestions/>
 
         </>
     );
