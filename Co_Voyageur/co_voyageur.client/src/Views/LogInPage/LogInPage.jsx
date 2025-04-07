eed5490b5b43fecd4e8c076038510fa2f21d9a0e
benoit <bntnowak@outlook.com>
2025-04-07 09:56:04 +0200

    FEAT/ add inputs LogIn

--- Co_Voyageur/co_voyageur.client/src/Views/LogInPage/LogInPage.jsx
+++ Co_Voyageur/co_voyageur.client/src/Views/LogInPage/LogInPage.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-
+import NavBar from '../../Components/NavBar/NavBar';
 const LogInPage = () => {
     return (
-        <div>
-            <h1>Connexion</h1>
-            <form>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" required />
-                </div>
-                <button type="submit">Log In</button>
-            </form>
-        </div>
+        <>
+        <NavBar></NavBar>
+            <h1>Log In</h1>
+            <input type="text" placeholder='Username'/>
+            <input type="password" placeholder='Password'/>
+            <button>Log In</button>
+        </>
     );
 };
 
