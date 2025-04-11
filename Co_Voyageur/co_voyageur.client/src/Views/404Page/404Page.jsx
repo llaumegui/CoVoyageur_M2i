0bceabbc53e8b7cb939b6c179e3040a0aae47ed7
benoit <bntnowak62@gmail.com>
2025-04-11 16:08:00 +0200

    FIX/ refactor css style for 404 page

--- Co_Voyageur/co_voyageur.client/src/Views/404Page/404Page.jsx
+++ Co_Voyageur/co_voyageur.client/src/Views/404Page/404Page.jsx
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom';
 import './404Page.css';
 const NotFoundPage = () => {
     return (
-        <div className="not-found-page">
-            <img src="https://http.cat/404" alt="404-not-found" title='404-error-httpcats.org' />
-            <p className="gui-le-déglingo">Guillaume a dit : "c'est bientôt le week end</p>
-            <p className="developper-message">La page demandée n'existe pas... MIAOU?!</p>
-            <Link to="/" className="button-back">Retour à l'accueil</Link>
+        <div className="not-found">
+            <img src="https://http.cat/404" alt="404-not-found"/>
+            <h1 className="not-found__message">La page demandée n'existe pas... MIAOU?!</h1>
+            <button><Link to={"/"}>Retour à la page d'accueil</Link></button>
         </div>
     );
 };
